b95fb464f0c5fb01e9b64f39a7c2da8bbb20953f
J. Mauricio Benitez <microsistemasprogramados@gmail.com>
2022-10-20 01:12:27 -0300

    Se cambió diseño y se agregaron animaciones en el header y controles mediante js

--- script/index.js
+++ script/index.js
@@ -1,5 +1,27 @@
-let text=document.querySelector("#text");
-window.addEventListener("scroll",()=>{
-  let value=window.scrollY;
-  text.style.marginBottom=value*2+"px";
+let text = document.querySelector("#text");
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+  text.style.marginBottom = value * 2 + "px";
 })
+
+let flag = 0;
+const element = document.querySelector('.menuOculto');
+
+const elemento = document.querySelector('.lihamb');
+
+elemento.addEventListener("click", () => {
+ 
+  if (flag == 0) {
+    element.classList.remove("oculto");
+  }
+  flag=1;
+  element.classList.forEach(el => {
+
+    element.classList.toggle("visible");
+    element.classList.toggle('animate__backInRight');
+    element.classList.toggle('animate__backOutRight');
+
+    element.classList.toggle("oculto");
+
+  })
+});
